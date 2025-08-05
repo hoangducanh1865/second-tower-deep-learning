5bd43c57b00762d2cf8cf840a63c21158973b359
namdeptraivcd <namhoang4d@gmail.com>
2025-08-06 00:44:27 +0700

    “Update”

--- d2l/function.py
+++ d2l/function.py
@@ -1,9 +1,10 @@
 import torch
 from torch import nn
 from torch.utils.data import DataLoader, TensorDataset
+from torchvision import datasets, transforms
 import random
 import matplotlib.pyplot as plt
-
+import sys
 # Thiết lập môi trường
 torch.manual_seed(42)
 
@@ -28,3 +29,25 @@ def sgd(params, lr, batch_size):
 def load_array(data_arrays, batch_size, is_train = True): # chia minibatch
     data_set = TensorDataset(*data_arrays)
     return DataLoader(data_set, batch_size, shuffle= is_train)
+
+def get_dataloader_workers(num_workers=4):
+    if sys.platform.startswith("win"):
+        return 0
+    else:
+        return num_workers
+
+def load_data_fashion_mnist(batch_size, resize = None):
+    trans = []
+
+    if resize:
+        trans.append(transforms.Resize(resize))
+    trans.append(transforms.ToTensor())
+    transform = transforms.Compose(transform)
+
+    mnist_train = datasets.FashionMNIST(root="../data", train = True, download = True, transform = transform)
+    mnist_test = datasets.FashionMNIST(root="../data", train = False, download = True, transform = transform)
+
+    train_iter = DataLoader(mnist_train, batch_size, shuffle=True, num_workers = get_dataloader_workers())
+    test_iter = DataLoader(mnist_test, batch_size, shuffle=False, num_workers = get_dataloader_workers())
+    
+    return train_iter, test_iter
