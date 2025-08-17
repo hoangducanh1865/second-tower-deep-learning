8b35b05b957d5c5599dbb0470b17eef74378da34
namdeptraivcd <namhoang4d@gmail.com>
2025-08-17 09:18:24 +0700

    “Update”

--- d2l/function.py
+++ d2l/function.py
@@ -51,3 +51,55 @@ def load_data_fashion_mnist(batch_size, resize = None):
     test_iter = DataLoader(mnist_test, batch_size, shuffle=False, num_workers = get_dataloader_workers())
     
     return train_iter, test_iter
+
+def accuracy (y_hat, y):
+    if y_hat.shape[1] > 1:
+        preds = y_hat.argmax(axis =1)
+    else:
+        preds = y_hat.round().int().squeeze()
+
+    return (preds == y).sum().item()
+
+def evaluate_accuracy (net, data_iter):
+    metric = Accumulator (2)
+    for X, y in data_iter:
+        metric.add(accuracy(net(X), y), y.size)
+    return metric[0] / metric[1]
+
+class Accumulator():
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float (b) for a, b in zip(self.data, args)]
+    
+    def reset(self):
+        self.data = [0] * len(self.data)
+
+    def __getitem__(self, i):
+        return self.data[i]
+    
+def train_epoch_ch3(net, train_iter, loss_fn, optimizer, device=None):
+    net.train()  
+    metric = Accumulator(3)  # [train_loss_sum, train_acc_sum, num_examples]
+
+    for X, y in train_iter:
+        if device:
+            X, y = X.to(device), y.to(device)
+
+        y_hat = net(X)
+        loss = loss_fn(y_hat, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        metric.add(
+            float(loss.sum()),            
+            accuracy(y_hat, y),           
+            y.size(0)                     
+        )
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+
+
