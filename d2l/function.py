b0f6f845a924f9a5c683d5988af8f8f7a677f8de
namdeptraivcd <namhoang4d@gmail.com>
2025-08-17 21:21:44 +0700

    “Update”

--- d2l/function.py
+++ d2l/function.py
@@ -1,112 +1,71 @@
-import torch
-from torch import nn
-from torch.utils.data import DataLoader, TensorDataset
-from torchvision import datasets, transforms
+import math
+import time
+import numpy as np
 import random
-import matplotlib.pyplot as plt
-import sys
-# Thiết lập môi trường
-torch.manual_seed(42)
+import torch
+from torch.utils import data
+from torchvision import transforms
+from d2l import torch as d2l
+class Timer:
+    def __init__ (self):
+        self.times = []
+        self.start ()
 
-def synthetic_data(w, b, num_examples):
-    X = torch.normal(0, 1, (num_examples, len(w)))
+    def stop(self):
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+    
+    def avg(self):
+        return sum(self.times) / len(self.times)
+    
+    def sum(self):
+        return sum(self.times)
+    
+    def cumsum(self):
+        return np.array(self.times).cumsum().tolist()
+
+def synthetic_data (w, b, num_examples):
+    X = torch.normal (0,1, (num_examples, len(w)))
     y = torch.matmul(X, w) + b
     y += torch.normal(0, 0.01, y.shape)
     return X, y.reshape((-1, 1))
 
-def linreg(X, w, b):
-    return torch.matmul(X, w) + b
-def sgd(params, lr, batch_size):
+def linreg(X,w, b):
+    return torch.matmul(X,w) + b
+
+def squared_loss (y_hat, y):
+    return (y_hat - y.reshape(y_hat.shape)) ** 2/2
+
+def sgd (params, lr, batch_size):
     with torch.no_grad():
         for param in params:
-            param -= lr * param.grad / batch_size  # Cập nhật inplace
-            param.grad.zero_()  # Xóa gradient
+            param -= lr * param.grad / batch_size
+            param.grad.zero_()
 
-def squared_loss(y_hat, y):
-    return ((y_hat - y.reshape(y_hat.shape))**2) /2
+def load_array (data_arrays, batch_size, is_train = True):
+    dataset = data.TensorDataset(dataset, batch_size, shuffle = is_train)
+    return data.DataLoader(dataset, batch_size, shuffle = is_train)
 
+def get_fashion_mnist_labels(labels):  #@save
+    """Return text labels for the Fashion-MNIST dataset."""
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
 
-def load_array(data_arrays, batch_size, is_train = True): # chia minibatch
-    data_set = TensorDataset(*data_arrays)
-    return DataLoader(data_set, batch_size, shuffle= is_train)
-
-def get_dataloader_workers(num_workers=4):
-    if sys.platform.startswith("win"):
-        return 0
-    else:
-        return num_workers
-
-def load_data_fashion_mnist(batch_size, resize = None):
-    trans = []
-
-    if resize:
-        trans.append(transforms.Resize(resize))
-    trans.append(transforms.ToTensor())
-    transform = transforms.Compose(trans)
-
-    mnist_train = datasets.FashionMNIST(root="../data", train = True, download = True, transform = transform)
-    mnist_test = datasets.FashionMNIST(root="../data", train = False, download = True, transform = transform)
-
-    train_iter = DataLoader(mnist_train, batch_size, shuffle=True, num_workers = get_dataloader_workers())
-    test_iter = DataLoader(mnist_test, batch_size, shuffle=False, num_workers = get_dataloader_workers())
-    
-    return train_iter, test_iter
-
-def accuracy (y_hat, y):
-    if y_hat.shape[1] > 1:
-        preds = y_hat.argmax(axis =1)
-    else:
-        preds = y_hat.round().int().squeeze()
-
-    return (preds == y).sum().item()
-
-def evaluate_accuracy (net, test_iter):
-    metric = Accumulator (2)
-    for X, y in test_iter:
-        metric.add(accuracy(net(X), y), y.size)
-    return metric[0] / metric[1]
-
-class Accumulator():
-    def __init__(self, n):
-        self.data = [0.0] * n
-
-    def add(self, *args):
-        self.data = [a + float (b) for a, b in zip(self.data, args)]
-    
-    def reset(self):
-        self.data = [0] * len(self.data)
-
-    def __getitem__(self, i):
-        return self.data[i]
-    
-def train_epoch_ch3(net, train_iter, loss_fn, optimizer, device=None):
-    net.train()  
-    metric = Accumulator(3)  # [train_loss_sum, train_acc_sum, num_examples]
-
-    for X, y in train_iter:
-        if device:
-            X, y = X.to(device), y.to(device)
-
-        y_hat = net(X)
-        loss = loss_fn(y_hat, y)
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        metric.add(
-            float(loss.sum()),            
-            accuracy(y_hat, y),           
-            y.size(0)                     
-        )
-    return metric[0] / metric[2], metric[1] / metric[2]
-
-
-
-def train_ch3(net, train_iter, test_iter, loss_fn, num_epochs, optimizer, device = None):
-    for epoch in range (num_epochs):
-        train_loss, train_acc = train_epoch_ch3(net, train_iter, loss_fn, optimizer, device)
-        test_acc = evaluate_accuracy(net, test_iter)
-
-
-
+def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):  #@save
+    """Plot a list of images."""
+    figsize = (num_cols * scale, num_rows * scale)
+    _, axes = d2l.plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, (ax, img) in enumerate(zip(axes, imgs)):
+        if torch.is_tensor(img):
+            # Tensor Image
+            ax.imshow(img.numpy())
+        else:
+            # PIL Image
+            ax.imshow(img)
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        if titles:
+            ax.set_title(titles[i])
+    return axes
--- main.py
+++ main.py
@@ -1,48 +1,19 @@
+# `ToTensor` converts the image data from PIL type to 32-bit floating point
+# tensors. It divides all numbers by 255 so that all pixel values are between
+# 0 and 1
 import torch
-from torch import nn
-import random
-import matplotlib.pyplot as plt
-from d2l import synthetic_data, linreg, squared_loss, sgd
-torch.manual_seed(42)
+import torchvision
+from torch.utils import data
+from torchvision import transforms
+from d2l import torch as d2l
+from d2l import function as f
 
+d2l.use_svg_display()
+trans = transforms.ToTensor()
+mnist_train = torchvision.datasets.FashionMNIST(
+    root="../data", train=True, transform=trans, download=True)
+mnist_test = torchvision.datasets.FashionMNIST(
+    root="../data", train=False, transform=trans, download=True)
 
-
-# initialize parameters
-true_w = torch.tensor([2.0, -3.4])
-true_b = 4.2
-
-w = torch.normal(0, 0.01, size=(2, 1), requires_grad=True)
-b = torch.zeros(1, requires_grad=True)
-
-lr = 0.03
-num_epochs = 3
-batch_size = 10
-
-features, labels = synthetic_data(true_w, true_b, 1000)
-
-
-
-
-# init minibatch
-def data_iter(batch_size, features, labels):
-    num_examples = len(features)
-    indices = list(range(num_examples))
-    random.shuffle(indices)
-    for i in range(0, num_examples, batch_size):
-        batch_indices = torch.tensor(indices[i: min(i + batch_size, num_examples)])
-        yield features[batch_indices], labels[batch_indices]
-
-
-# Training
-for epoch in range(num_epochs):
-    for X, y in data_iter(batch_size, features, labels):
-        l = squared_loss(linreg(X, w, b), y)
-        l.sum().backward()
-        sgd([w, b], lr, batch_size)
-    with torch.no_grad():
-        train_l = squared_loss(linreg(features, w, b), labels)
-        print(f'epoch {epoch + 1}, loss {train_l.mean():.6f}')
-
-# In kết quả
-print(f'Estimated w: {w.reshape(-1).detach()}')
-print(f'Estimated b: {b.item()}')
+X, y = next(iter(data.DataLoader(mnist_train, batch_size=18)))
+f.show_images(X.reshape(18, 28, 28), 2, 9, titles=f.get_fashion_mnist_labels(y))
