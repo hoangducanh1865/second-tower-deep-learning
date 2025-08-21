e83a37007c6c416ba5ec0bf2b9850984533a9387
namdeptraivcd <namhoang4d@gmail.com>
2025-08-21 21:19:31 +0700

    Update

--- main.py
+++ main.py
@@ -1,32 +1,19 @@
-import torch 
-import matplotlib.pyplot as plt
-from save import function as f
+import torch
 from torch import nn
-from d2l import torch as d2l
-batch_size = 256
-train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+from save import function as f
+num_inputs, num_hiddens, num_outputs = 28, 256, 10
 
-num_inputs, num_outputs, num_hiddens = 784, 10, 256
-W1 = nn.Parameter(torch.randn(num_inputs, num_hiddens, requires_grad=True))
-b1 = nn.Parameter(torch.zeros(num_hiddens, requires_grad = True))
-W2 = nn.Parameter(torch.randn(num_hiddens, num_outputs, requires_grad = True))
-b2 = nn.Parameter(torch.zeros(num_outputs, requires_grad = True))
+net = nn.Sequential(nn.Linear(num_inputs, num_hiddens),
+                    nn.ReLU(),
+                    nn.Linear(num_hiddens, num_outputs))
 
-params = [W1, b1, W2, b2]
+def init_weights(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, std =0.01)
 
-def relu(X):
-    a = torch.zeros_like(X)
-    return torch.max(X, a)
-
-def net(X):
-    X = X.reshape((-1, num_inputs))
-    H = relu(X@W1 + b1)
-    return (H@W2 + b2)
-
-loss = nn.CrossEntropyLoss(reduction='none')
-
-num_epochs, lr = 10, 0.1
-updater = torch.optim.SGD(params, lr = lr)
+net.apply(init_weights)
+batch_size, lr, num_epochs = 256, 0.1, 10
+loss = nn.CrossEntropyLoss(reduction = "none")
+updater = torch.optim.SGD(net.parameters(), lr = lr)
+train_iter, test_iter = f.load_data_fashion_mnist(batch_size= batch_size)
 f.train_ch3(net, train_iter, test_iter, loss, num_epochs, updater)
-plt.show()
-f.predict_ch3(net, test_iter)
